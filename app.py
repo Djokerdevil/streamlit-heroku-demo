35bdc2ae15ce2b7b6492bbc24a85b2bbd8bb3d67
অভিলাষ পাল Abhilash Pal <abhilashpal_anish@srmuniv.edu.in>
2021-12-31 11:39:45 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,91 @@
+from datasets import load_dataset
+import streamlit as st
+import pandas as pd 
+from googletrans import Translator 
+import torch
+from transformers import PegasusForConditionalGeneration, PegasusTokenizer
+
+translator = Translator()
+
+model_name = 'tuner007/pegasus_paraphrase'
+torch_device = 'cuda' if torch.cuda.is_available() else 'cpu'
+tokenizer = PegasusTokenizer.from_pretrained(model_name)
+model = PegasusForConditionalGeneration.from_pretrained(model_name).to(torch_device)
+
+def get_response(input_text,num_return_sequences,num_beams):
+  batch = tokenizer([input_text],truncation=True,padding='longest',max_length=60, return_tensors="pt").to(torch_device)
+  translated = model.generate(**batch,max_length=60,num_beams=num_beams, num_return_sequences=num_return_sequences, temperature=1.5)
+  tgt_text = tokenizer.batch_decode(translated, skip_special_tokens=True)
+  return tgt_text
+
+def get_qa_pair(file):
+	df = pd.read_csv(file,sep="\t")
+	a = df.sample(1)
+	return {
+	"text" : a["text"][0],
+	"question" : a["question"][0],
+	"answer" : a["answer"][0]
+	}
+
+
+
+def main():
+	"""Adaptive Education"""
+	st.title("Reading Comprehension with AI capabilities")
+
+	#normal RC
+
+	# authentication logic + get which class student belongs to : high, mid, low 
+	stud_class = "high" 
+
+	if st.checkbox("Get New Passage"):
+		cqa = get_qa_pair(stud_class+".tsv") # high.tsv is saved
+
+
+		st.subheader("Context : ")
+		st.markdown(cqa['text'])
+
+		st.subheader("Question : ")
+		st.markdown(cqa['question'])	
+
+		message1 = st.text_area("Enter your answer","Type Here")
+
+		if st.button("Check"):
+
+			st.subheader("Your Answer : ")
+			st.text(message1)
+			score = 0
+			if message1.lower() in cqa["answer"].lower():
+				score = 1
+			st.text("Score : ")
+			if score:
+				st.text("Correct!")
+				st.subheader("Full Answer : ")
+				t.text(cqa['answer'])
+			else: 
+				st.text("Incorrect!")
+				st.subheader("Actual Answer : ")
+				st.text(cqa['answer'])
+
+	if st.checkbox("Translate to Hindi"):
+		txt = st.text_area("Enter here to Translate","Type Here")
+		out = translator.translate(txt,dest="hi")
+		st.subheader("Hindi Text : "+out.text)
+
+	if st.checkbox("Paraphrase Given Sentence"):
+		txt2 = st.text_area("Enter here to Paraphrase","Type Here")
+		out2 = translator.translate(txt2,dest="hi")
+		st.json(out2)
+
+
+	st.sidebar.subheader("About This App")
+	st.sidebar.write("#Integrating AI and differentiated Data across studnet buckets, this is an attempt at using AI tools to enable English Language acquisition within a focussed group of 93 kids of a TFI classroom. ")
+	st.sidebar.info("The app is meant for the use of students and teachers of Grade 8, Holy Mother English School;Mumbai.")
+	st.sidebar.subheader("Created with ♥ by ")
+	st.sidebar.text("Brought to you by Debamita Samajdar, Abhilash Paul, and Honey Joshi.")
+
+
+
+
+if __name__ == '__main__':
+	main()	
