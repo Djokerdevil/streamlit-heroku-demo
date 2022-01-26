03022a82f5870ffb9e597777a7d31df84383d54b
Djokerdevil <46436748+Djokerdevil@users.noreply.github.com>
2022-01-26 22:40:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from datasets import load_dataset
 import streamlit as st
 import pandas as pd 
 from googletrans import Translator 
@@ -10,8 +9,8 @@ from fuzzywuzzy import fuzz,process
 # from transformers import PegasusForConditionalGeneration, PegasusTokenizer
 
 state = session_state.get(question_number=0)
-
 translator = Translator()
+
 
 # model_name = 'tuner007/pegasus_paraphrase'
 # torch_device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -27,7 +26,7 @@ translator = Translator()
 @st.cache
 def get_qa_pair(file,rand):
 	df = pd.read_csv(file,sep="\t",lineterminator='\n')
-	a = df.sample().reset_index()
+	a = df.sample(1).reset_index()
 	return {
 	"text" : a["text"][0],
 	"question" : a["question"][0],
@@ -63,6 +62,8 @@ def main():
 	st.write('You selected:', option)
  
 	if option == "Reading Comprehension":
+		
+	
 		st.text("Question Number : " +str(state.question_number))
 		st.text("Your Score : " +str(scoreM))
 		cqa = get_qa_pair(stud_class+".tsv",state.question_number) # high.tsv is saved
@@ -76,25 +77,46 @@ def main():
 
 		message1 = st.text_area("Enter your answer","Type Here")
 		# a = st.selectbox('Answer:', ["Please select an answer","Confirm Answer"])
-		a = st.radio("Confirm : ", ["Answering","Confirm!"])
-
-		if a != "Answering":
-			st.subheader("Your Answer : ")
+		# a = st.radio("Confirm : ", ["Answering","Confirm!"])
+		
+		# if a != "Answering":
+		if st.button("Check"):
+			st.subheader("Your Answer :")
 			st.text(message1)
 			score = 0
 			if fuzz.ratio(message1.lower(),cqa["answer"].lower()) > 75:
 				score = fuzz.ratio(message1.lower(),cqa["answer"].lower()) 
 			st.text("Score : "+str(score))
-			if score:
-				st.text("Correct!")
-				st.subheader("Full Answer : ")
-				st.text(cqa['answer'])
-			else: 
-				st.text("Incorrect!")
-				st.subheader("Actual Answer : ")
-				st.text(cqa['answer'])
+			# if score:
+			# 	st.text("Correct!")
+			# 	st.subheader("Full Answer : ")
+			# 	st.text(cqa['answer'])
+			# else: 
+			# 	st.text("Incorrect!")
+			# 	st.subheader("Actual Answer : ")
+			# 	st.text(cqa['answer'])
 		
+			#writing to score.tsv
+			fp = open("score.tsv","a")
+			fp.write(cqa["text"].strip()+"\t"+cqa["question"].strip()+"\t"+cqa["answer"].strip()+"\t"+message1.strip()+"\t"+str(score)+"\n")
+			fp.close()
+			 	
+			
 		
+		if st.button("Show Answers"):
+				try:
+					df = pd.read_csv("score.tsv",sep="\t")
+					json = df.to_json(orient="records")
+					# json2 = {}
+					# for i in range(len(json["Text"])):
+					# 	json2[i] = {}
+					
+					# for i in range(len(json["Text"])):
+					# 	json2[i][]
+					
+					st.json(json)
+				except: 
+					st.text("No Questions Answered Yet!")
 		if st.button("Get New Question"):
 			state.question_number+=1
 			scoreM +=score
@@ -133,9 +155,9 @@ def main():
 
 	st.sidebar.subheader("About This App")
 	st.sidebar.write("#Integrating AI and differentiated Data across student buckets, this is an attempt at using AI tools to enable English Language acquisition amongst a focussed group of 93 kids of a TFI classroom. ")
-	st.sidebar.info("The app is meant for the use of students and Teach For India Fellows of Grade 8, Holy Mother English School;Mumbai.")
-	st.sidebar.subheader("Created with ♥ by ")
-	st.sidebar.text("Brought to you by Debamita Samajdar, Abhilash Paul, and Honey Joshi.")
+	st.sidebar.info("The app is meant for the use of students and TFI Fellows of Grade 8, Holy Mother English School;Mumbai.")
+	st.sidebar.subheader("Created with ♥ ")
+	st.sidebar.text("By Debamita S., Abhilash P., and Honey J.")
 
 
 
